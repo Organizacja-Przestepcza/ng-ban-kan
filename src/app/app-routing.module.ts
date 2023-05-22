7d5df796614438d25b086a9d38a1834b0e29d600
magda_m <magdamachacka@gmail.com>
2023-05-22 22:10:43 +0200

    router-outlet works!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KanbanComponent } from './kanban/kanban.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const routes: Routes = [
-  { path: 'kanban', component: KanbanComponent},
+  { path: 'kanban-component', component: KanbanComponent},
+  { path: 'welcome-page-component', component: WelcomePageComponent},
+  { path: '',   redirectTo: 'welcome-page-component', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KanbanComponent } from './kanban/kanban.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    KanbanComponent
+    KanbanComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
