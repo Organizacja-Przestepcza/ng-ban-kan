4b44fded7a76693bddb0b6facb78cc40ad92a0ad
magda_m <magdamachacka@gmail.com>
2023-05-22 21:46:44 +0200

    new component

---
+++ src/app/kanban/kanban.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KanbanComponent } from './kanban.component';
+
+describe('KanbanComponent', () => {
+  let component: KanbanComponent;
+  let fixture: ComponentFixture<KanbanComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [KanbanComponent]
+    });
+    fixture = TestBed.createComponent(KanbanComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
