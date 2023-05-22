8aa1775c76a7e348193a301281377376f9468a43
magda_m <magdamachacka@gmail.com>
2023-05-22 21:54:42 +0200

    some things to fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KanbanComponent } from './kanban/kanban.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'kanban-component', component: KanbanComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
