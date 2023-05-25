adf111ee5676bc73ad1f57ee1c3bf28fd7add461
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-05-25 14:12:30 +0200

    added ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { KanbanComponent } from './kanban/kanban.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
